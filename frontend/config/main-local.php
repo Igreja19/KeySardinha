151346269d67699c11a68f6e1925c68053ed10c0
Igreja19 <2232332@my.ipleiria.pt>
2025-10-21 18:26:44 +0100

    trying fix main-local

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -3,23 +3,9 @@
 $config = [
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'aDEKGGQCbAPZk5vM_Wz-mqoFRqldvH4y',
         ],
     ],
 ];
-
-// Só ativa debug e gii em ambiente de desenvolvimento
-if (getenv('YII_ENV') === 'dev' || defined('YII_ENV_DEV') && YII_ENV_DEV) {
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => \yii\debug\Module::class,
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => \yii\gii\Module::class,
-    ];
-}
 
 return $config;
