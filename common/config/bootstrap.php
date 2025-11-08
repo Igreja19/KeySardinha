fdaa7b7f34882552c8e296c8427ce750edc7881c
Igreja19 <2232332@my.ipleiria.pt>
2025-11-08 15:42:53 +0000

    Ignorar ficheiros de configuração de ambiente

--- common/config/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
-
-Yii::setAlias('@web', 'http://projeto/frontend/web/site/index'); // Substitua pela URL desejada
-Yii::setAlias('@webroot', dirname(dirname(__DIR__)) . '/frontend/web/site/index'); // Caminho físico correspondente
